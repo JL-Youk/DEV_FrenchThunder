826ab0b6131c6a6b16f2a4fcd8540c96385d8c83
JL <jeanlouis02300@gmail.com>
2016-10-08 12:40:48 +0200

    correction titre

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
-    <title>P:Society</title>
+    <title>French Thunder</title>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--Import jQuery before materialize.js-->
